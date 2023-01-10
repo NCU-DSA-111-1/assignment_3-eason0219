fd9fd49242e5dd3377819dbc2ad8db94f36b00bd
eason0219 <easonyeh0219@gmail.com>
2023-01-10 18:04:13 +0800

    arcd

---
+++ Arcd/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/easonyeh/arcd/tests
+# Build directory: /home/easonyeh/arcd/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(codec_tests "/home/easonyeh/arcd/tests/codec_tests")
+set_tests_properties(codec_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/easonyeh/arcd/tests/CMakeLists.txt;10;add_test;/home/easonyeh/arcd/tests/CMakeLists.txt;0;")
